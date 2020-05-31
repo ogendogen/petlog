d9b6c1735ebabf11fa161ee3ffb1b0c2051d3ffc
ogendogen <kosmos500@interia.pl>
2020-06-01 01:32:49 +0200

    Created AnimalsWindow

---
+++ PetLog/AnimalsWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Database.Models;
+
+namespace PetLog
+{
+    /// <summary>
+    /// Interaction logic for AnimalsWindow.xaml
+    /// </summary>
+    public partial class AnimalsWindow : Window
+    {
+        public User User { get; }
+        public AnimalsWindow(User user)
+        {
+            InitializeComponent();
+            User = user;
+
+            MessageBox.Show($"Witaj {User.Name} {User.Surname}");
+        }
+    }
+}
