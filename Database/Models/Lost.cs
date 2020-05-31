89d138ccd2c4ca695a9a4d50be00d65c4f4f298b
ogendogen <kosmos500@interia.pl>
2020-05-31 16:34:37 +0200

    Added Lost model

---
+++ Database/Models/Lost.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Database.Models
+{
+    public class Lost
+    {
+        public int ID { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+    }
+}
