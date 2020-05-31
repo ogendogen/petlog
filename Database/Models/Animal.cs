5ae61550512d9133c6b18185b18032d4f7767cd1
ogendogen <kosmos500@interia.pl>
2020-05-31 17:09:34 +0200

    Added animal model

---
+++ Database/Models/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Database.Models
+{
+    public class Animal
+    {
+        public int ID { get; set; }
+        public AnimalType Type { get; set; }
+        public DateTime BirthDate { get; set; }
+        public DateTime JoinDate { get; set; }
+        public ICollection<Vaccination> Vaccinations { get; set; }
+        public int Chip { get; set; }
+        public string Description { get; set; }
+        public Adopter? Adopter { get; set; }
+        public Death? DeathInfo { get; set; }
+        public Lost? LostInfo { get; set; }
+        public string State { get; set; }
+        public string Treatments { get; set; }
+    }
+}
