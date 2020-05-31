c401fce9d0dbfe2b983d58fc6d997398b70dc954
ogendogen <kosmos500@interia.pl>
2020-06-01 01:29:55 +0200

    Changed MainWindow to LoginWindow

--- PetLog/LoginWindow.xaml.cs
+++ PetLog/LoginWindow.xaml.cs
@@ -17,16 +17,34 @@ using Database;
 namespace PetLog
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for LoginWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class LoginWindow : Window
     {
         public UsersManager UsersManager { get; set; }
-        public MainWindow()
+        public LoginWindow()
         {
             UsersManager = new UsersManager();
             WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
             InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string login = LoginTextBox.Text;
+            string password = PasswordPasswordBox.Password;
+
+            var user = UsersManager.Login(login, password);
+            if (user == null)
+            {
+                MessageBox.Show("Niepoprawny login lub hasło!", "Uwaga", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            
+            AnimalsWindow animalsWindow = new AnimalsWindow(user);
+            animalsWindow.Show();
+
+            Close();
         }
     }
 }
