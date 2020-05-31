6826cf8c303186f74b91008e78a3a1625e2e2023
ogendogen <kosmos500@interia.pl>
2020-05-31 16:56:06 +0200

    Added AnimalType enum

---
+++ Database/Models/AnimalType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Database.Models
+{
+    public enum AnimalType
+    {
+        Dog,
+        Cat,
+        Other
+    }
+}
