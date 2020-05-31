03a5cc151f36fd3488e68d3b2093cdeb018c4e60
ogendogen <kosmos500@interia.pl>
2020-05-31 16:33:53 +0200

    Added Adopter model

---
+++ Database/Models/Adopter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Database.Models
+{
+    public class Adopter
+    {
+        public int ID { get; set; }
+        [MaxLength(32)]
+        public string Name { get; set; }
+        [MaxLength(32)]
+        public string Surname { get; set; }
+        [MaxLength(64)]
+        public string Email { get; set; }
+        public int Telephone { get; set; }
+    }
+}
