3f376e550681e9db0553c52fca69db15c6e86ff0
ogendogen <kosmos500@interia.pl>
2020-05-31 19:21:51 +0200

    Created UsersManager with basic methods

---
+++ Database/UsersManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using Database.Models;
+using Microsoft.EntityFrameworkCore.Internal;
+
+namespace Database
+{
+    public class UsersManager
+    {
+        internal VetContext Vet { get; set; }
+        public User Login(string name, string password)
+        {
+            string hashedPassword = HashMD5(password);
+            return Vet.Users.FirstOrDefault(user => user.Name == name && user.Password == hashedPassword);
+        }
+
+        public User AddNewUser(string username,
+                               string password,
+                               string name,
+                               string surname,
+                               bool isAdmin)
+        {
+            string hashedPassword = HashMD5(password);
+            if (Vet.Users.Any(user => user.Username == username))
+            {
+                throw new Exception("Taki login jest już zajęty!");
+            }
+            
+            User user = new User(){
+                Username = username,
+                Password = hashedPassword,
+                Name = name,
+                Surname = surname,
+                IsAdmin = isAdmin
+            };
+
+            return Vet.Users.Add(user).Entity;
+        }
+
+        private string HashMD5(string password)
+        {
+            byte[] encodedPassword = new UTF8Encoding().GetBytes(password);
+            byte[] hash = ((HashAlgorithm)CryptoConfig.CreateFromName("MD5")).ComputeHash(encodedPassword);
+            return BitConverter.ToString(hash).Replace("-", string.Empty).ToLower();
+        }
+    }
+}
