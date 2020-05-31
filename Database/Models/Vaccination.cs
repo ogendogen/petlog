3321199860402a4d774a8f5e865e811c588d3f0c
ogendogen <kosmos500@interia.pl>
2020-05-31 16:31:09 +0200

    Added Vaccination model

---
+++ Database/Models/Vaccination.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Database.Models
+{
+    public class Vaccination
+    {
+        public int ID { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+    }
+}
