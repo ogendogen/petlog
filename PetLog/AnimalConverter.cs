c66279ca98ba8b2198393b20bdeae86ee3fb71ab
ogendogen <kosmos500@interia.pl>
2020-06-02 21:41:44 +0200

    Enum converter for animal type

---
+++ PetLog/AnimalConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+using Database.Models;
+
+namespace PetLog
+{
+    public class AnimalConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string originalEnum = ((AnimalType)value).ToString();
+
+            switch(originalEnum)
+            {
+                case "Dog":
+                    return "Pies";
+
+                case "Cat":
+                    return "Kot";
+
+                case "Other":
+                    return "Inny";
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
