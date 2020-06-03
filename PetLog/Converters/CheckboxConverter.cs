3c575e3f840445d2f5eb5f26899fd6e2008ce938
ogendogen <kosmos500@interia.pl>
2020-06-04 00:05:50 +0200

    Added Death/Lost columns

--- PetLog/Converters/CheckboxConverter.cs
+++ PetLog/Converters/CheckboxConverter.cs
@@ -6,7 +6,7 @@ using System.Windows.Data;
 
 namespace PetLog
 {
-    public class IsAdoptedConverter : IValueConverter
+    public class CheckboxConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
