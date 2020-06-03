ea7d1ceefdd3f00d3f428b3dc0ce42450c579a93
ogendogen <kosmos500@interia.pl>
2020-06-04 00:32:03 +0200

    Added VaccinationConverter

---
+++ PetLog/Converters/VaccinationConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using Database.Models;
+
+namespace PetLog.Converters
+{
+    public class VaccinationConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            HashSet<Vaccination> vaccinations = (HashSet<Vaccination>)value;
+            if (vaccinations.Count > 0)
+            {
+                DateTime dt = vaccinations.OrderByDescending(vacc => vacc.Date).First().Date;
+                return dt.ToString();
+            }
+
+            return "Brak";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
