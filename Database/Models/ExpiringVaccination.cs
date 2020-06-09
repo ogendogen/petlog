45000390c674f063e8f6398a608601e04f7c0823
ogendogen <kosmos500@interia.pl>
2020-06-09 20:19:04 +0200

    Created model for view

---
+++ Database/Models/ExpiringVaccination.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Database.Models
+{
+    public class ExpiringVaccination
+    {
+        public string Name { get; set; }
+        public DateTime VaccinationDate { get; set; }
+        public DateTime ExpireDate { get; set; }
+    }
+}
